14bebd781bae113c16caf6403345e771355b97b8
SimonasEndriulaitis <122284975+SimonasEndriulaitis@users.noreply.github.com>
2023-07-24 18:30:08 +0300

    commit

--- twitter-clone/src/Login.jsx
+++ twitter-clone/src/Login.jsx
@@ -1,24 +1,52 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 
 export const Login = (props) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    
-    const handleSubmit = () => {
-        console.log(email);
-    }
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
 
-    return (
-        <div className="auth-form-container">
-     <form className="login-form" onSubmit={handleSubmit}>
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePassChange = (event) => {
+    setPass(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email);
+  };
+
+  return (
+    <div className="auth-form-container">
+      <h2>Login</h2>
+      <form className="login-form" onSubmit={handleSubmit}>
         <label htmlFor="email">Email</label>
-        <input value={email} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+        <input
+          value={email}
+          type="email"
+          placeholder="youremail@gmail.com"
+          id="email"
+          name="email"
+          onChange={handleEmailChange} // Add onChange handler
+        />
         <label htmlFor="password">Password</label>
-        <input value={pass} type="password" placeholder="********" id="password" name="password" />
+        <input
+          value={pass}
+          type="password"
+          placeholder="********"
+          id="password"
+          name="password"
+          onChange={handlePassChange} // Add onChange handler
+        />
         <button type="submit">Log in</button>
-     </form>
-     <button className="Link-button" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
-     </div>
-    )
-}
+      </form>
+      <button
+        className="Link-button"
+        onClick={() => props.onFormSwitch("register")}
+      >
+        Don't have an account? Register here.
+      </button>
+    </div>
+  );
+};
--- twitter-clone/src/Register.jsx
+++ twitter-clone/src/Register.jsx
@@ -1,28 +1,65 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 
 export const Register = (props) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    const[name, setName] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email);
-    }
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
 
-    return (
-        <div className="auth-form-container">
-     <form className="register-form" onSubmit={handleSubmit}>
-        <label htmlFor="name">name</label>
-        <input value={name} name="name" id="name" placeholder="username" />
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePassChange = (event) => {
+    setPass(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email);
+  };
+
+  return (
+    <div className="auth-form-container">
+      <h2>Register</h2>
+      <form className="register-form" onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          value={name}
+          name="name"
+          id="name"
+          placeholder="username"
+          onChange={handleNameChange} // Add onChange handler
+        />
         <label htmlFor="email">Email</label>
-        <input value={email} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+        <input
+          value={email}
+          type="email"
+          placeholder="youremail@gmail.com"
+          id="email"
+          name="email"
+          onChange={handleEmailChange} // Add onChange handler
+        />
         <label htmlFor="password">Password</label>
-        <input value={pass} type="password" placeholder="********" id="password" name="password" />
-        <button type="submit">Log in</button>
-     </form>
-     <button className="Link-button" onClick={() => props.onFormSwitch('login')}>Already have an account? Log in here.</button>
-     </div>
-    )
-}
+        <input
+          value={pass}
+          type="password"
+          placeholder="********"
+          id="password"
+          name="password"
+          onChange={handlePassChange} // Add onChange handler
+        />
+        <button type="submit">Register</button>
+      </form>
+      <button
+        className="Link-button"
+        onClick={() => props.onFormSwitch("login")}
+      >
+        Already have an account? Log in here.
+      </button>
+    </div>
+  );
+};
